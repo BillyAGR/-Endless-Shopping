ae5d562b9c8940d20c70c2fd2522c300dfbacdf9
sao <wllmgr@hotmail.com>
2024-09-05 16:13:36 -0500

    Ajustes OrdersCard

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, CalendarDaysIcon, ShoppingBagIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon, CalendarDaysIcon, ShoppingBagIcon } from '@heroicons/react/24/outline'
 import { dateFormatte } from '../../utils'
 
 const OrdersCard = props => {
@@ -9,18 +9,18 @@ const OrdersCard = props => {
         <div className='flex justify-between items-center mb-3 border border-black rounded-lg p-4 w-80' >
             <div className='flex justify-between w-full'>
                 <div className='flex flex-col'>
-                    <div className="flex gap-1">
-                        <CalendarDaysIcon className="h-6 w-6 text-black cursor-pointer" />
+                    <div className='flex gap-1'>
+                        <CalendarDaysIcon className='h-6 w-6 text-black cursor-pointer' />
                         <span className='font-light'>{dateFormatte()}</span>
                     </div>
-                    <div className="flex gap-1">
-                        <ShoppingBagIcon className="h-6 w-6 text-black cursor-pointer" />
+                    <div className='flex gap-1'>
+                        <ShoppingBagIcon className='h-6 w-6 text-black cursor-pointer' />
                         <span className='font-light'>{totalProducts} articles</span>
                     </div>
                 </div>
-                <p className="flex items-center gap-2">
+                <p className='flex items-center gap-2'>
                     <span className='font-medium text-2xl'>${totalPrice} </span>
-                    <ChevronRightIcon className="h-6 w-6 text-black cursor-pointer" />
+                    <ChevronRightIcon className='h-6 w-6 text-black cursor-pointer' />
                 </p>
             </div>
         </div>
